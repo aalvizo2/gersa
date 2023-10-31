f15f8be2b7d3eba1c92103a4262a7422223c535b
Victor261995 <mad_civ@yahoo.com>
2023-10-30 22:39:13 -0300

    mejorado de navbar y logos

--- gersaFront/src/App.js
+++ gersaFront/src/App.js
@@ -2,14 +2,24 @@ import logo from './logo.svg';
 import './App.css';
 import Login from './Pages/Login/Login'
 import Navbar from './Components/Navbar/Navbar';
+import { Route } from 'react-router-dom';
+
+
+
+
+
+
 
 function App() {
   return (
+   
     <div>
-        <Navbar />
+     
+      <Navbar />
     <Login />
   
     </div>
+    
   );
 }
 
